656db21ee38bf4779236603cd91aeaf2e0f50fef
edz <edz@edzdeMacBook-Air-10.local>
2020-10-09 16:48:47 +0800

    lang

--- basicNote/Note/1009/test4.js
+++ basicNote/Note/1009/test4.js
@@ -4,7 +4,7 @@ let i,j;
 
 for(i = 0; i < 33; i++) red[i] = i+1;
 for(i = 0,j; i < 6;i++){
-    j = Math.ceil(Math.random()*(33-i)); 
+    j = Math.floor(Math.random()*(32-i)); 
     s[i] = red[j];
     for(let k = j; k < red.length; k++) red[k] = red[k+1];
     red.length--;
